53fd3ebc87139df35c24338d5595356e05809e6d
Daniel Gluskin <daniel@starkware.co>
2022-08-30 17:48:42 +0300

    export StarkUtils from package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {StarkExClient} from './lib';
 import {getLogger, printPackageInfo, setLogLevel} from './utils';
 
+export {StarkExUtils} from './lib';
+
 declare const __VERSION__: string;
 declare const __NAME__: string;
 declare const __NODE_ENV__: string;
