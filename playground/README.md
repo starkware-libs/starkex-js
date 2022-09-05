d39ce0a880f1ca36516d412b2facef24d25d13a6
Daniel Gluskin <110768512+danielgluskin-starkware@users.noreply.github.com>
2022-09-05 16:15:48 +0300

    chore(3153819259): webpack watch rebuild (#17)

---
+++ playground/README.md
@@ -0,0 +1,15 @@
+### Playground
+
+In order to run webpack in 'watch' mode:
+
+`npm run start`
+
+Run node package from a JS file:
+
+`npm run playground:js`
+
+Run node package from a TS file (assuming ts-node installed globally):
+
+`npm run playground:ts`
+
+Run browser package from a HTML page: Open client.html with your browser. 
---
+++ playground/server.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+import StarkExAPI from '../dist/node';
+import fs from 'fs';
+import path from 'path';
+
+const readCert = certType =>
+  fs.readFileSync(path.resolve(`certs/user.${certType}`));
+
+const starkExAPI = new StarkExAPI({
+  endpoint: 'https://gw.playground-v2.starkex.co'
+});
+
+// const starkExAPIWithCerts = new StarkExAPI({
+//   endpoint: 'https://playground.starkex.co',
+//   certs: {
+//     cert: readCert('crt'),
+//     key: readCert('key')
+//   }
+// });
+
+(async function () {
+  console.log('Server running');
+  console.log(await starkExAPI.gateway.isAlive());
+  // console.log(await starkExAPIWithCerts.gateway.isAlive());
+})();
