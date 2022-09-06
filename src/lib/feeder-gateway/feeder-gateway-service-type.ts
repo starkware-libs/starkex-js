a79f90fa0b055fe89fdb263c53597d69f14b6baf
Daniel Gluskin <daniel@starkware.co>
2022-09-06 20:44:50 +0300

    feeder gateway v2

--- src/lib/feeder-gateway/feeder-gateway-service-type.ts
+++ src/lib/feeder-gateway/feeder-gateway-service-type.ts
@@ -2,7 +2,8 @@ enum FeederGatewayServiceType {
   GET_BATCH_ENCLOSING_IDS = 'get_batch_enclosing_ids',
   GET_BATCH_IDS = 'get_batch_ids',
   GET_BATCH_INFO = 'get_batch_info',
-  GET_LAST_BATCH_ID = 'get_last_batch_id'
+  GET_LAST_BATCH_ID = 'get_last_batch_id',
+  GET_PREV_BATCH_ID = 'get_prev_batch_id'
 }
 
 export {FeederGatewayServiceType};
--- src/lib/feeder-gateway/feeder-gateway.ts
+++ src/lib/feeder-gateway/feeder-gateway.ts
@@ -5,7 +5,7 @@ import {FeederGatewayServiceType} from './feeder-gateway-service-type';
 
 class FeederGateway extends GatewayBase {
   constructor(config: StarkExClientConfig) {
-    super(config, '/feeder_gateway');
+    super(config, '/v2/feeder_gateway');
   }
 
   public getBatchEnclosingIds(batchId: number): Promise<number[]> {
@@ -29,6 +29,12 @@ class FeederGateway extends GatewayBase {
 
   public getLastBatchId(): Promise<number> {
     return this.makeRequest(`${FeederGatewayServiceType.GET_LAST_BATCH_ID}`);
+  }
+
+  public getPrevBatchId(batchId: number): Promise<number> {
+    return this.makeRequest(
+      `${FeederGatewayServiceType.GET_PREV_BATCH_ID}?batch_id=${batchId}`
+    );
   }
 }
 
